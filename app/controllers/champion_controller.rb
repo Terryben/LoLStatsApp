df56b4c9fa389b45f77180dd3d76a6bb2557be74
Ben Terry <Ben18181820@gmail.com>
2020-02-24 20:24:53 -0700

    Figuring out CSS. Have a basic style for my pages. Very simple

--- app/controllers/champion_controller.rb
+++ app/controllers/champion_controller.rb
@@ -7,6 +7,7 @@ class ChampionController < ApplicationController
 		#must include the "select("*") or it will not return values from the joined table. Commented out section is a differet and kinda worse way to write this code
 		@champion = Champion.select("*").left_outer_joins(:champion_positional_stats).where("champions.id = ?", params[:id])
 		#@champion = Champion.select("*").joins("INNER JOIN champion_positional_stats ON champions.id = champion_positional_stats.champion_id").where("champions.id = #{params[:id]}")	
+		@total_matches = Match.count
 	end
 	
 	def new
